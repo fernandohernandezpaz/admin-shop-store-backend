81e4fb5bba3f04684c2bec54ed2583b2830d8640
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2023-09-20 07:41:07 -0600

    adding unit test for brands controller

--- src/api/v1/brands/brands.controller.spec.ts
+++ src/api/v1/brands/brands.controller.spec.ts
@@ -2,19 +2,83 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
 
-describe('BrandsController', () => {
+describe('BrandsController tests', () => {
   let controller: BrandsController;
+  const mockBrandsService = {
+    findOne: jest.fn((id) => ({
+      id,
+      name: 'Apple',
+    })),
+    findAll: jest.fn().mockImplementation(() => [
+      { id: 1, name: 'Dell' },
+      { id: 2, name: 'Apple' },
+    ]),
+    create: jest.fn((dto) => ({
+      id: Date.now(),
+      ...dto,
+    })),
+    update: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto,
+    })),
+    remove: jest.fn().mockImplementation((_) => 'This brand was delete'),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BrandsController],
       providers: [BrandsService],
-    }).compile();
+    })
+      .overrideProvider(BrandsService)
+      .useValue(mockBrandsService)
+      .compile();
 
     controller = module.get<BrandsController>(BrandsController);
   });
 
-  it('should be defined', () => {
+  it('Should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should create a brand', () => {
+    const dto = { name: 'Dell' };
+    expect(controller.create(dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
+    expect(mockBrandsService.create).toHaveBeenCalledWith(dto);
+  });
+
+  it('Should update a brand', () => {
+    const dto = { name: 'Dell' };
+    expect(controller.update(1, dto)).toEqual({
+      id: expect.any(Number),
+      ...dto,
+    });
+    expect(mockBrandsService.create).toHaveBeenCalledWith(dto);
+  });
+
+  it('Should retrieve a brand', () => {
+    const id = 1;
+    expect(controller.findOne(id)).toEqual({
+      id,
+      name: 'Apple',
+    });
+    expect(mockBrandsService.findOne).toHaveBeenCalledWith(id);
+  });
+
+  it('Should get a list of brands', () => {
+    const paginationDto = {};
+    expect(controller.findAll(paginationDto)).toEqual([
+      { id: 1, name: 'Dell' },
+      { id: 2, name: 'Apple' },
+    ]);
+    expect(mockBrandsService.findAll).toHaveBeenCalled();
+  });
+
+  it('Should delete a brand', () => {
+    const id = 1;
+    expect(controller.remove(id)).toEqual('This brand was delete');
+    expect(mockBrandsService.remove).toHaveBeenCalledWith(id);
   });
 });
